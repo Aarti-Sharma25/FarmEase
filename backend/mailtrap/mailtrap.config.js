25ac8fa6f30d160051c577f7b1587a0eb85df6ae
Aarti-Sharma25 <aartisharmaindian25@gmail.com>
2026-04-15 23:43:31 +0530

    Initial commit

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -4,12 +4,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// export const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST, 
+//   port: process.env.SMTP_PORT,
+//   auth: {
+//     user: process.env.SMTP_USER, 
+//     pass: process.env.SMTP_PASS, 
+//   },
+// });
 export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST, 
-  port: process.env.SMTP_PORT,
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false, // important
   auth: {
-    user: process.env.SMTP_USER, 
-    pass: process.env.SMTP_PASS, 
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
