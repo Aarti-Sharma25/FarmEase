b94ab64ffa123cc1f5294a211a83ed5ba04144a1
Aarti Sharma <aartisharmaindian25@gmail.com>
2026-07-03 16:46:01 +0530

    Update authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 //type frontend\src\store\rentalStore.jsxconst API_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api/auth" : "/api/auth";
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api/auth" : "https://farmease-rku8.onrender.com/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api/auth" : "https://farmease-backend-8sjs.onrender.com";
 axios.defaults.withCredentials = true;
 
 export const useAuthStore = create((set) => ({
